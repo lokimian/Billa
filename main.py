d4e552642e21bf9979e60543bc189ecfdd3f92b9
lokimian <fayyazqadeer@gmail.com>
2023-12-15 19:34:11 +1300

    Password Change Command added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ help_desk = Helpdesk()
 
 print(help_desk.create_ticket('User1', 'Network issue'))
 print(help_desk.create_ticket('User2', 'Software problem'))
-
+print(help_desk.create_ticket('User2', 'Pass word change'))
 print(help_desk.view_tickets())
 #comment
 
