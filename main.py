414c287fbd4d526f839212d05edb7c4ee0b3fabb
lokimian <fayyazqadeer@gmail.com>
2023-12-19 17:34:44 +1300

    Names Change of ticket users

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Helpdesk import Helpdesk
 help_desk = Helpdesk()
 
-print(help_desk.create_ticket('User1', 'Network issue'))
-print(help_desk.create_ticket('User2', 'Software problem'))
-print(help_desk.create_ticket('User2', 'Pass word change'))
+print(help_desk.create_ticket('Tom', 'Network issue'))
+print(help_desk.create_ticket('Mel', 'Software problem'))
+print(help_desk.create_ticket('Jo', 'Pass word change'))
 print(help_desk.close_ticket)
 print(help_desk.view_tickets())
 #comment
