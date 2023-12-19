e26a338603f67d953c599b4e2f3d0669bbade8a7
lokimian <fayyazqadeer@gmail.com>
2023-12-19 20:36:07 +1300

    Ticket sorting

--- Helpdesk.py
+++ Helpdesk.py
@@ -12,8 +12,7 @@ class Helpdesk:
             return 'No tickets available.'
         ticket_list = '\n'.join([f'Ticket #{ticket_id} - User: {ticket["user"]}, Issue: {ticket["issue"]}, Status: {ticket["status"]}' for ticket_id, ticket in self.tickets.items()])
         return ticket_list
-    def total_tickets(self):
-        total_tickets=len(open_tickets)
+
 
     def close_ticket(self, ticket_id):
         if ticket_id in self.tickets:
@@ -21,5 +20,20 @@ class Helpdesk:
             return f'Ticket #{ticket_id} closed successfully.'
         else:
             return f'Ticket #{ticket_id} not found.'
+
+
+ # Function to calculate total open and closed tickets
+    def calculate_ticket_stats(ticket_list):
+        open_tickets = 0
+        closed_tickets = 0
+
+        for ticket in ticket_list:
+            if ticket["status"] == "open":
+                open_tickets += 1
+            elif ticket["status"] == "closed":
+                closed_tickets += 1
+
+        return open_tickets, closed_tickets
+
         #comment-Readme added
         print("Done")
--- main.py
+++ main.py
@@ -4,9 +4,15 @@ help_desk = Helpdesk()
 print(help_desk.create_ticket('Tom', 'Network issue'))
 print(help_desk.create_ticket('Mel', 'Software problem'))
 print(help_desk.create_ticket('Jo', 'Pass word change'))
-print(help_desk.open_tickets)
+
 print(help_desk.close_ticket)
 print(help_desk.view_tickets())
+
+# Calculate and print the results
+open_count, closed_count = calculate_ticket_stats(tickets)
+
+print(f"Total Open Tickets: {open_count}")
+print(f"Total Closed Tickets: {closed_count}")
 #comment
 
 
