e22c8d6ac4359925234e1fdd42d2fa135b3acd99
lokimian <fayyazqadeer@gmail.com>
2023-12-19 17:04:55 +1300

    Changes status of open ticket

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ help_desk = Helpdesk()
 print(help_desk.create_ticket('User1', 'Network issue'))
 print(help_desk.create_ticket('User2', 'Software problem'))
 print(help_desk.create_ticket('User2', 'Pass word change'))
-print(help_desk.close_ticket(1))
+print(help_desk.close_ticket)
 print(help_desk.view_tickets())
 #comment
 
